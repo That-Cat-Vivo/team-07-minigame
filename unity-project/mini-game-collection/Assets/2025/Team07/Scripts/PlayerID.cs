840580c5089c49cf5a208d7867a8c642e4271047
That-Cat-Vivo <eman3001@yahoo.com>
2025-11-10 13:10:53 -0500

    Add player ID enumeration

---
+++ unity-project/mini-game-collection/Assets/2025/Team07/Scripts/PlayerID.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    // Start is called before the first frame update
+    public enum PlayerID
+    {
+        Player1 = 0,
+        Player2 = 1,
+    }
